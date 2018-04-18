42f2d4cec8ca1abceec254ea4e5f78641b37e823
Ken Lee <wewert@gmail.com>
2018-04-18 14:29:14 -0600

    Updates

---
+++ assets/img/index.js
@@ -0,0 +1,17 @@
+// import max10 from './madmax10';
+// import max20 from './madmax20';
+// import max30 from './madmax30';
+// import max40 from './madmax40';
+// import max50 from './madmax50';
+// import max60 from './madmax60';
+// import max70 from './madmax70';
+// import max80 from './madmax80';
+//
+// export default [
+//   {img: max10, id: 0, name: "timmy todd" },
+//   max20,
+//   max30,
+//   max40,
+//   max50,
+//   max60
+// ];
